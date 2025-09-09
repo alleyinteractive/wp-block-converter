fea21ff969c444214add2d7401ba19ed07fbf407
Sean Fisher <srtfisher@gmail.com>
2025-09-09 16:32:58 -0400

    Fix issue where parent images were being sideloaded when sideload_images was set to false

--- tests/Feature/BlockConverterTest.php
+++ tests/Feature/BlockConverterTest.php
@@ -117,6 +117,19 @@ class BlockConverterTest extends TestCase {
 		$this->assertRequestSent( 'https://alley.com/wp-content/uploads/2022/01/Screen-Shot-2022-01-19-at-2.51.37-PM.png', 1 );
 	}
 
+	public function test_image_with_sideloading_disabled(): void {
+		$html = <<<'HTML'
+<img src="https://example.org/image.jpg" alt="" />
+HTML;
+
+		$result = ( new Block_Converter( $html, false ) )->convert();
+
+		$this->assertEquals(
+			'<!-- wp:image --><figure class="wp-block-image"><img src="https://example.org/image.jpg" alt=""></figure><!-- /wp:image -->',
+			$result,
+		);
+	}
+
 	public static function image_dataprovider(): array {
 		$url = wp_upload_dir()['url'];
 
